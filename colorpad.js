c8e83f8ea93bbcb958329d500de2274fe7d9acfe
John Love <john.love4@gmail.com>
2017-06-30 14:03:27 +0300

    add input and dynamically create grid

--- colorpad.js
+++ colorpad.js
@@ -1,3 +1,11 @@
+var $grid = document.getElementById("grid");
+
+// function createGrid() {
+// 	for(var i = 0; i < $grid.value; i++){
+// 		$(".container").append('<div class="this">' + "div" + '</div>');
+// 	}
+// }
+
 function randomColor() {
 	//pick a red from 0 - 255
 	//rounds down, so multiply by 256
@@ -11,7 +19,16 @@ function randomColor() {
 }
 
 $(document).ready(function(){
-	$('.this').mouseover(function(){
+	$("#newGrid").on("click", function(){
+		$(".gridContainer").children().remove();
+		for(var i = 0; i < $grid.value; i++){
+			$('<div class="this">' + '</div>').appendTo(".gridContainer");
+			for(var j = 0; j < $grid.value; j++){
+				$('<div class="this">' + '</div>').appendTo(".gridContainer");
+			}
+		}
+	});
+	$('.gridContainer').on("mouseover", "div", function(){
 		$(this).css("background-color", randomColor());
 	})
 });
